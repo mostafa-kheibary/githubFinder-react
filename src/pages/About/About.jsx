0125995ed94476e6ce68fe9b7221c3a0bf4525a1
mostafa <mostafamnbvcxz5@gmail.com>
2022-02-20 23:53:30 +0330

    finish about page

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,8 +1,36 @@
-const About = ()=>{
-  return(
-    <div className="about">
-      <h1>about</h1>
+import Button from '../../components/Button/Button';
+import { useNavigate } from 'react-router-dom';
+import './About.css';
+const About = () => {
+  const navigate = useNavigate();
+  const changePage = () => {
+    navigate('/');
+  };
+  return (
+    <div className='about-container'>
+      <h1 className='about-title'>Github finder</h1>
+      <h4 className='about-author'>Mostafa Kheibary</h4>
+      <p className='about-info'>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elpor et mattis
+        pharetra. Penatibus purus habitant pharetra molestie. Tortor quis sed
+        senectus scelerisque neque. Lorem ipsum dolor sit amet, consectetur
+        adipiscing elpor et mattis pharetra. Penatibus purus habitant pharetra
+        molestie. Tortor quis sed senectus scelerisque neque
+      </p>
+      <div className='about-social'>
+        <a
+          href='https://linkedin.com/in/mostafa-kheibary'
+          className='fa-brands fa-linkedin about-social__icon'
+        ></a>
+        <a
+          href='https://github.com/mostafa-kheibary'
+          className='fa-brands fa-github about-social__icon'
+        ></a>
+      </div>
+      <div className='about-button-page'>
+        <Button onClick={changePage}>back to Home</Button>
+      </div>
     </div>
-  )
-}
+  );
+};
 export default About;
