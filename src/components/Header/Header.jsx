0f9330774bf02449f2cd2bae9edeba4cad335831
mostafa <mostafamnbvcxz5@gmail.com>
2022-02-20 22:44:08 +0330

    finish header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,9 @@
+import './Header.css'
 const Header = () => {
   return (
     <header className='header'>
       <div className='header__logo'>
-        <i class='fa-brands fa-github-alt'></i>
-        <h1>Github finder</h1>
+        <i class='fa-brands fa-github-alt header-logo'></i>
       </div>
       <div className='header__nav'>
         
