1e21c6e36529b310bee5255772b173cf726dcbe8
mostafa <mostafamnbvcxz5@gmail.com>
2022-03-11 09:51:41 +0330

    edit about page

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,6 +1,7 @@
 import Button from '../../components/Button/Button';
 import { useNavigate } from 'react-router-dom';
 import './About.css';
+
 const About = () => {
   const navigate = useNavigate();
   const changePage = () => {
@@ -11,17 +12,21 @@ const About = () => {
       <h1 className='about-title'>Github finder</h1>
       <h4 className='about-author'>Mostafa Kheibary</h4>
       <p className='about-info'>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elpor et mattis
-        pharetra. Penatibus purus habitant pharetra molestie. Tortor quis sed
-        senectus scelerisque neque. Lorem ipsum dolor sit amet, consectetur
-        adipiscing elpor et mattis pharetra. Penatibus purus habitant pharetra
-        molestie. Tortor quis sed senectus scelerisque neque
+        github finder app create using React, context and reducer for state
+        managing.
+        if you want to collaborate in this project and expend it you can fork it.
       </p>
       <div className='about-social'>
-        <a className='about-social__link' href='https://linkedin.com/in/mostafa-kheibary'>
+        <a
+          className='about-social__link'
+          href='https://linkedin.com/in/mostafa-kheibary'
+        >
           <i className='fa-brands fa-linkedin about-social__icon'></i>
         </a>
-        <a className='about-social__link' href='https://github.com/mostafa-kheibary'>
+        <a
+          className='about-social__link'
+          href='https://github.com/mostafa-kheibary'
+        >
           <i className='fa-brands fa-github about-social__icon'></i>
         </a>
       </div>
